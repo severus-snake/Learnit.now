874ac9de3a088927a3322590d790909407ec0e4e
Eric Andrade <eandrade21@gmail.com>
2017-03-27 19:42:47 -0700

    trying something new and refactoring

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
- function firepadInit(roomName, ACEdom) {
-    // Initialize Firebase.
-    this.config = {
-        apiKey: "AIzaSyC-UmzZh1QBe61yc9cdAoEITJfN3v_P0ZQ",
-        authDomain: "fir-firsttest-5635d.firebaseapp.com",
-        databaseURL: "https://fir-firsttest-5635d.firebaseio.com",
-        storageBucket: "fir-firsttest-5635d.appspot.com",
-        messagingSenderId: "931278023084"
-    };
-    firebase.initializeApp(this.config);
+// Initialize Firebase.
+var config = {
+    apiKey: "AIzaSyC-UmzZh1QBe61yc9cdAoEITJfN3v_P0ZQ",
+    authDomain: "fir-firsttest-5635d.firebaseapp.com",
+    databaseURL: "https://fir-firsttest-5635d.firebaseio.com",
+    storageBucket: "fir-firsttest-5635d.appspot.com",
+    messagingSenderId: "931278023084"
+};
 
-    // Get Firebase Database reference.
-    this.firepadRef = firebase.database().ref(roomName);
+firebase.initializeApp(config);
+var firebaseDb = firebase.database();
 
+function setup(roomName) {
+    // Set Firebase Database reference.
+    var fireOb = firebaseDb.ref(roomName);
+    firepadInit('firepad', fireOb);
+}
+
+function firepadInit(ACEdom, fireOb) {
     // Create Ace editor.
-    this.editor = ace.edit(ACEdom);
+    var editor = ace.edit(ACEdom);
 
-    // Create Firepad.
-    this.ACEdom = Firepad.fromACE(this.firepadRef, this.editor);
+// Create Firepad.
+    Firepad.fromACE(fireOb, editor);
  }
 
- function createRoom(roomName) {
+ /*function createRoom(roomName) {
      new firepadInit(roomName, 'firepad');
- }
+ }*/
 
  $('button').click(function(event) {
+     var clicks = event;
+     console.log(clicks);
      var roomName = $(this).text();
-     createRoom(roomName);
+     setup(roomName);
  });
 
-$('button').dblclick(function () {
+/*$('button').dblclick(function () {
     location.reload(true);
-});
+});*/
